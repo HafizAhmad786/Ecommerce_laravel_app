e3375063c3c3566db01c78a5c36a24bbda250711
Hafiz Ahmad <mk9379118@gmail.com>
2026-01-19 00:13:07 +0500

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+
+    public function loginPage()
+    {
+        return view("auth.login");
+    }
+
+    public function registerPage()
+    {
+        return view("auth.register");
+    }
+
+    public function login(Request $request)
+    {
+       $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            if(Auth::user()->role == "buyer"){
+                return redirect()->route("buyerdashboard");
+            }else{
+                return redirect()->route("sellerdashboard");
+            }
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function register(Request $request){
+
+
+       $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            "name" => $validated['name'],
+            "email" => $validated['email'],
+            "password" => Hash::make($validated['password'])
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Welcome!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+//for view
+Route::get("/login", [AuthController::class, "loginPage"])->name("loginPage");
+Route::get("/register", [AuthController::class, "registerPage"])->name("registerPage");
+
+Route::get("/seller/dashboard", function () {
+    return view('seller.dashboard');
+})->name("sellerdashboard")->middleware(['auth','role:seller']);
+
+Route::get("/buyer/dashboard", function () {
+    return view('buyer.dashboard');
+})->name("buyerdashboard")->middleware(['auth','role:buyer']);
+
+
+// for logic
+Route::post("/login", [AuthController::class, "login"])->name("login");
+Route::post("/register", [AuthController::class, "register"])->name("register");
